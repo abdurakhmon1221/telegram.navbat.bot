0a0fa0ef6ff16f9fb8043123798df2e7037676c0
abdurakhmon1221 <abdurahmonyuldashev1221@gmail.com>
2026-01-03 18:19:01 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
-let queue = [];
+let queues = {};
 
 bot.onText(/\/start/, (msg) => {
   bot.sendMessage(msg.chat.id, "Salom! /join bilan navbatga qo‘shil.");
